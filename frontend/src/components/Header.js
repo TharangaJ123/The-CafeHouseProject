9587037463701bd29ad0ef9352b36c6c8694ecd7
TharangaJ123 <chathuratharanga076@gmail.com>
2024-12-05 22:08:06 +0530

    creating some components,css files and upload image files

---
+++ frontend/src/components/Header.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import './Header.css'
+
+export default function Header(){
+  return(
+    <div>
+
+      <nav class="navbar navbar-expand-lg navbar-light bg-light d-flex align-items-center text-center">
+        
+
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul class="navbar-nav mx-auto">
+          <a class="navbar-brand mx-auto p-lg-2" href="#">The Cafe House</a>
+            <li class="nav-item active p-lg-1">
+              <a class="nav-link " href="#">Home</a>
+            </li>
+            <li class="nav-item p-lg-1">
+              <a class="nav-link" href="#">Menu</a>
+            </li>
+            <li class="nav-item p-lg-1">
+              <a class="nav-link" href="#">Reservations</a>
+            </li>
+            <li class="nav-item p-lg-1">
+              <a class="nav-link" href="#">Contact</a>
+            </li>
+       
+
+          </ul>
+        </div>
+      </nav>
+
+    </div>
+  );
+}
